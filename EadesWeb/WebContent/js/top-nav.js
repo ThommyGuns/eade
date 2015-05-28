93bf748f1d9c26e8bc380bae5c3d52c51717b253
levi-lewis <lewisls10@bonaventure.edu>
2015-05-27 22:37:50 -0400

    Changes to the top-nav that sets active styling for the current page

--- EadesWeb/WebContent/js/top-nav.js
+++ EadesWeb/WebContent/js/top-nav.js
@@ -1,20 +1,47 @@
-/**
- * 
- */
-document.write('<div class="custom-header">\
-		<div class="container">\
-		<div class="col-lg-12">\
-		<div class="pull-left">\
-			<a href="#"><img class="pull-left" src="assets/EF.jpg"></a>\
+//document.write(
+//		'<div class="custom-header">\
+//		<div class="container">\
+//		<div class="col-lg-12">\
+//		<div class="pull-left">\
+//			<a href="#"><img class="pull-left" src="assets/EF.jpg"></a>\
+//			<h3 class="main-title pull-right">Eades Fitness 24/7</h3>\
+//		</div>\
+//		<div class="pull-right">\
+//			<ul class="nav navbar-nav">\
+//				<li><a href="Home.html">Home</a></li>\
+//				<li><a href="#">Memberships</a></li>\
+//				<li><a href="#">Nutrition</a></li>\
+//				<li class="active"><a href="personal_trainers.html">Trainers</a></li>\
+//				<li><a href="#">Workouts</a></li>\
+//				<li class="dropdown"><a class="dropdown-toggle"\
+//					data-toggle="dropdown" href="#">Events<span class="caret"></span></a>\
+//					<ul class="dropdown-menu">\
+//						<li><a href="#">2015 USBF</a></li>\
+//						<li><a href="#">2015 Powerlifting Contest</a></li>\
+//						<li><a href="#">Stick Wrestling</a></li>\
+//					</ul>\
+//				</li>\
+//			</ul>\
+//		</div>\
+//		</div>\
+//	</div>\
+//</div>'
+//);
+//
+document.write('\
+ <nav class="navbar navbar-default">\
+   <div class="container">\
+	     <div class="navbar-header">\
+			<a href="Home.html" class="navbar-brand"><img class="pull-left brand-image" src="assets/EF.jpg"></a>\
 			<h3 class="main-title pull-right">Eades Fitness 24/7</h3>\
-		</div>\
-		<div class="pull-right">\
-			<ul class="nav">\
-				<li><a href="Home.html">Home</a></li>\
-				<li><a href="#">Memberships</a></li>\
-				<li><a href="#">Nutrition</a></li>\
-				<li><a href="personal_trainers.html">Trainers</a></li>\
-				<li><a href="#">Workouts</a></li>\
+		 </div>\
+		 <div>\
+			<ul class="nav navbar-nav pull-right">\
+				<li id="home"><a href="Home.html">Home</a></li>\
+				<li id="memberships"><a href="#">Memberships</a></li>\
+				<li id="nutrition"><a href="#">Nutrition</a></li>\
+				<li id="trainers"><a href="personal_trainers.html">Trainers</a></li>\
+				<li id="workouts"><a href="#">Workouts</a></li>\
 				<li class="dropdown"><a class="dropdown-toggle"\
 					data-toggle="dropdown" href="#">Events<span class="caret"></span></a>\
 					<ul class="dropdown-menu">\
@@ -24,7 +51,9 @@ document.write('<div class="custom-header">\
 					</ul>\
 				</li>\
 			</ul>\
-		</div>\
-		</div>\
+		 </div>\
 	</div>\
-</div>');
+	</div>\
+  </nav>\
+'		
+);
