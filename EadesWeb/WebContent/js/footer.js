8bd34e3102d2c2ffb755a11460807116e4e561ac
levi-lewis <lewisls10@bonaventure.edu>
2015-05-29 22:01:00 -0400

    Footer js

---
+++ EadesWeb/WebContent/js/footer.js
@@ -0,0 +1,7 @@
+document.write('\
+		<div class="footer navbar navbar-default navbar-fixed-bottom">\
+		<div class="container">\
+			<p class="navbar-text pull-left">Website by GunzNLube</p>\
+		</div>\
+	</div>\
+');
